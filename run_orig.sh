eb9fa5189c90f6b180da644c4ba5b7ad9203bdff
Yannick Simon <ysimonx@MacBook-Air-M5-de-Yannick.local>
2026-04-28 18:29:41 +0200

    x

--- run_orig.sh
+++ run_orig.sh
@@ -3,4 +3,6 @@ python3.14 -m venv ./venv
 source ./venv/bin/activate
 pip install -r requirements.txt
 playwright install chromium
+rm .env
+ln -s ~/Documents/secrets/factures2time/env .env
 python scripts/run_now.py
